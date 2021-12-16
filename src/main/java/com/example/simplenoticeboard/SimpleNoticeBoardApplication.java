74d6fbd0ffb2e94dc05c7aa78b762b05e8d2fa7c
cori <cori.jang@bunjang.co.kr>
2021-12-16 20:58:29 +0900

    Initial commit

---
+++ src/main/java/com/example/simplenoticeboard/SimpleNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplenoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleNoticeBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplenoticeboard/SimpleNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplenoticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
